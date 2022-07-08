0ef87ac6d27fe09b65cc17d332255019fd4ba0ad
Subham <bairagisubham369@gmail.com>
2022-07-08 14:37:02 +0530

    some minor adjustments

--- VA_screenshot.py
+++ VA_screenshot.py
@@ -6,4 +6,5 @@ def takeShot():
     captured_frame = pyautogui.screenshot()
     captured_frame.save(r"C:\Users\NoThisIsSubham\Pictures\Screenshots\Screenshot1" + datetime.now().strftime("Screenshot taken on %a, %I %M %p, %B %Y")+ ".png")
     say("screen was successful.")
-takeShot()
+
+
